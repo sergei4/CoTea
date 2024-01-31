0bdced5db9480c1831c6c45f9660f7d51ac25e6b
Mikhail Eremkin <mikhail.eremkin@umbrellait.com>
2024-01-31 10:15:56 +0300

    Update version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+sed -ir "s/^[#]*\s*version=.*/version=$1/" gradle.properties
 git add . && git commit -m "Update version" &&
 git tag $1 &&
 git push origin && git push origin --tags
